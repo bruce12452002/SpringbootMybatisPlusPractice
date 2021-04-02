fb10e570d86a2e392bb94bf2e4e1756b95d30fb2
bruce <bruce>
2021-04-02 23:47:15 +0800

    add rollback 注釋

--- src/main/java/bruce/home/demo/service/impl/AnimalServiceImpl.java
+++ src/main/java/bruce/home/demo/service/impl/AnimalServiceImpl.java
@@ -18,18 +18,22 @@ public class AnimalServiceImpl extends ServiceImpl<AnimalMapper, Animal> impleme
     private AnimalMapper animalMapper;
 
     @Override
-    @Transactional//(rollbackFor = Exception.class)
+    @Transactional//(rollbackFor = Exception.class) // 非檢查(RunTimeException) 預設會回滾，而檢查例外預設不會回滾，都想回滾可以加 rollbackFor
     public int insertAnimal() throws CloneNotSupportedException {
-        Animal animal = Animal.builder()
-                .name("牛")
-                .price(100)
-                .date(LocalDateTime.now()).build();
-        int count = animalMapper.insert(animal);
-        System.out.println(count);
-        // int i = 1 / 0; // 會回滾
+        try {
+            Animal animal = Animal.builder()
+                    .name("牛")
+                    .price(100)
+                    .date(LocalDateTime.now()).build();
+            int count = animalMapper.insert(animal);
+            System.out.println(count);
+//            int i = 1 / 0; // RunTimeException 會回滾
+            throw new IndexOutOfBoundsException(); // 預設 RunTimeException 會回滾，但如果 catch 到的話就走 catch
+        } catch (IndexOutOfBoundsException e) { // 1/0 的例外是 ArithmeticException，接收不到，還是會走 Spring 的註解，會回滾
+            System.out.println("xxxxxxxxxxxxxxxxxx");
+        }
 
-        // try catch 事務就失效了，但使用 throws 可以，但要注意是不是檢查例外，非檢查(RunTimeException) 預設會回滾，而檢查例外預設不會回滾
-        xxx(); // 不會回滾，想回滾要加上 rollbackFor = Exception.class 才會回滾
+//        xxx(); // 使用 throws 時，還是看是不是 RunTimeException，如果不是，預設不會回滾，想回滾要加上 rollbackFor = Exception.class 才會回滾
         return 0;
     }
 
